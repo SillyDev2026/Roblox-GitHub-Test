eab5e1736089e1b5a78a38da9616e1130caeec48
SillyDev2026 <kramerlance797@gmail.com>
2026-06-04 00:51:53 -0800

    Sync from Roblox Studio

--- Modules/Wow.module.lua
+++ Modules/Wow.module.lua
@@ -1,12 +1,1 @@
-local UserTest = {
-	Coins = 0,
-	Rebirths = 0,
-	Prestiges = 0,
-	Wow = true,
-	function = funciton(false)
-		return false
-	end,
-	function false = true
-}
-
-print(UserTest)
+local Replicated = game:GetService('ReplicatedStorage')
