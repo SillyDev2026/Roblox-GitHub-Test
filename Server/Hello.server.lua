81477a3600d21eee807cae82ed667dfe613eff70
SillyDev2026 <kramerlance797@gmail.com>
2026-06-04 22:52:24 -0800

    Sync from Roblox Studio

--- Server/Hello.server.lua
+++ Server/Hello.server.lua
@@ -1,6 +1,3 @@
-local Replicated = game:GetService('ReplicatedStorage')
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
-local Modules = ReplicatedStorage.Modules
-local Mathmodule = require(Modules.Math.module)
+local Usler = {}
 
-print(Mathmodule.add(11234, 3))
+print(Usler)
